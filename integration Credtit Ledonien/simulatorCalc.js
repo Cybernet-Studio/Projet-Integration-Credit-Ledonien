2364572c0be9c2b406d6260846576c3b79902a99
Cybernet <arthurGt.pro@gmail.com>
2021-08-26 16:53:01 +0200

    simulateur

--- integration Credtit Ledonien/simulatorCalc.js
+++ integration Credtit Ledonien/simulatorCalc.js
@@ -6,14 +6,23 @@ let valueAmount=valueMonthlyPayments=valueTime=0;
         document.getElementById("amount").addEventListener("keyup",  function (){
             valueAmount=fieldAmount.value;
             console.log("amount "+valueAmount);
+            if (valueAmount<=500&&valueAmount>=7500){
+
+            }
         });
 
         document.getElementById("monthlyPayments").addEventListener("keyup",  function (){
             valueMonthlyPayments=fieldMonthlyPayments.value;
             console.log("monthlyPayments "+valueMonthlyPayments);
+            if (valueMonthlyPayments<=10&&valueMonthlyPayments>=12000){
+                
+            }
         });
         
         document.getElementById("time").addEventListener("keyup",  function (){
             valueTime=fieldTime.value;
             console.log("time "+valueTime);
+            if (valueTime<=6&&valueTime>=84){
+                
+            }
         });
