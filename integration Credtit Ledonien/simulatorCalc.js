05001c0ae4c1e8e9ba2468b238eeee75a3baf879
Cybernet <arthurGt.pro@gmail.com>
2021-08-27 15:19:48 +0200

    SimulatorTranslate

--- integration Credtit Ledonien/simulatorCalc.js
+++ integration Credtit Ledonien/simulatorCalc.js
@@ -3,7 +3,17 @@ let fieldMonthlyPayments=document.getElementById("monthlyPayments");
 let fieldTime=document.getElementById("time");
 let valueAmount=valueMonthlyPayments=valueTime=0;
 
-        document.getElementById("amount").addEventListener("keyup",  function (){
+function openCalc() {
+    if (document.getElementById("test").style = "width: 70vh") {
+    }
+}
+function closeCalc() {
+    if (document.getElementById("test").style = "width: 0") {
+    }
+}
+
+
+        document.getElementById("amount").addEventListener("keyup",function (){
             valueAmount=fieldAmount.value;
             console.log("amount "+valueAmount);
             if (valueAmount<=500&&valueAmount>=7500){
@@ -11,7 +21,7 @@ let valueAmount=valueMonthlyPayments=valueTime=0;
             }
         });
 
-        document.getElementById("monthlyPayments").addEventListener("keyup",  function (){
+        document.getElementById("monthlyPayments").addEventListener("keyup",function(){
             valueMonthlyPayments=fieldMonthlyPayments.value;
             console.log("monthlyPayments "+valueMonthlyPayments);
             if (valueMonthlyPayments<=10&&valueMonthlyPayments>=12000){
@@ -19,7 +29,7 @@ let valueAmount=valueMonthlyPayments=valueTime=0;
             }
         });
         
-        document.getElementById("time").addEventListener("keyup",  function (){
+        document.getElementById("time").addEventListener("keyup",function(){
             valueTime=fieldTime.value;
             console.log("time "+valueTime);
             if (valueTime<=6&&valueTime>=84){
